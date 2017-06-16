219016ebf4b0b66975a837462d4c3899e9167725
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-06-16 15:44:36 -0500

    redundant super long code script3

---
+++ script3.js
@@ -0,0 +1,81 @@
+var f = 'Daphne'
+    console.log ('Now ' + [f] + ' jumps in' + '!')
+    for (var i = 99; i > 0; i--) {
+        if (i > 2) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' lines of code in the file')
+        }
+        else if (i > 1) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' line of code in the file')
+        }
+        else if (i = 1) {
+            console.log ([i] + ' line of code in the file ' + [i] + ' line of code; ' + [f] + ' strikes one out, clears it all out, no more lines of code in the file.')
+        }
+        else {
+            console.log ('Next Friend Hops In!')
+        }
+    };
+
+var f = 'Fred'
+    console.log ('Now ' + [f] + ' jumps in' + '!')
+    for (var i = 99; i > 0; i--) {
+        if (i > 2) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' lines of code in the file')
+        }
+        else if (i > 1) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' line of code in the file')
+        }
+        else if (i = 1) {
+            console.log ([i] + ' line of code in the file ' + [i] + ' line of code; ' + [f] + ' strikes one out, clears it all out, no more lines of code in the file.')
+        }
+        else {
+            console.log ('Next Friend Hops In!')
+        }
+    };
+var f = 'Velma'
+    console.log ('Now ' + [f] + ' jumps in' + '!')
+    for (var i = 99; i > 0; i--) {
+        if (i > 2) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' lines of code in the file')
+        }
+        else if (i > 1) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' line of code in the file')
+        }
+        else if (i = 1) {
+            console.log ([i] + ' line of code in the file ' + [i] + ' line of code; ' + [f] + ' strikes one out, clears it all out, no more lines of code in the file.')
+        }
+        else {
+            console.log ('Next Friend Hops In!')
+        }
+    };
+var f = 'Shaggy'
+    console.log ('Now ' + [f] + ' jumps in' + '!')
+    for (var i = 99; i > 0; i--) {
+        if (i > 2) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' lines of code in the file')
+        }
+        else if (i > 1) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' line of code in the file')
+        }
+        else if (i = 1) {
+            console.log ([i] + ' line of code in the file ' + [i] + ' line of code; ' + [f] + ' strikes one out, clears it all out, no more lines of code in the file.')
+        }
+        else {
+            console.log ('Next Friend Hops In!')
+        }
+    };
+var f = 'Scooby'
+    console.log ('Now ' + [f] + ' jumps in' + '!')
+    for (var i = 99; i > 0; i--) {
+        if (i > 2) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' lines of code in the file')
+        }
+        else if (i > 1) {
+            console.log ([i] + ' lines of code in the file ' + [i] + ' lines of code; ' + [f] + ' strikes one out, clears it all out ' + [(i-1)] + ' line of code in the file')
+        }
+        else if (i = 1) {
+            console.log ([i] + ' line of code in the file ' + [i] + ' line of code; ' + [f] + ' strikes one out, clears it all out, no more lines of code in the file.')
+        }
+        else {
+            console.log ('Next Friend Hops In!')
+            }
+    };
